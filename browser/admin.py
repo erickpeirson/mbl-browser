d8b1f8e666a5d12ef57f6bd7bafd01066d018353
Erick Peirson <erick.peirson@asu.edu>
2015-10-12 09:12:51 -0700

    ready for Heroku

---
+++ browser/admin.py
@@ -0,0 +1,155 @@
+from django.contrib import admin
+from browser.models import *
+
+
+class PartOfInline(admin.TabularInline):
+    model = PartOf
+    readonly_fields = ('year', 'course', 'coursegroup')
+    extra = 0
+    can_delete = False
+    list_sort = ('year',)
+
+
+class AttendanceInline(admin.TabularInline):
+    model = Attendance
+    readonly_fields = ('year', 'person', 'course', 'role')
+    extra = 0
+    can_delete = False
+
+
+class AffiliationInline(admin.TabularInline):
+    model = Affiliation
+    readonly_fields = ('year', 'person', 'institution', 'position')
+    extra = 0
+    can_delete = False
+
+
+class LocationInline(admin.TabularInline):
+    model = Location
+    can_delete = False
+    extras = 0
+
+
+class AttendanceInline(admin.TabularInline):
+    model = Attendance
+    readonly_fields = ('person', 'course', 'role', 'year')
+    extra = 0
+    can_delete = False
+
+
+class InvestigatorInline(admin.TabularInline):
+    model = Investigator
+    readonly_fields = ('person', 'subject', 'role', 'year')
+    extra = 0
+    can_delete = False
+
+
+class LocalizationInline(admin.TabularInline):
+    model = Localization
+    readonly_fields = ('person', 'location', 'year')
+    extra = 0
+    can_delete = False
+
+## ModelAdmins ##
+
+class PersonAdmin(admin.ModelAdmin):
+    fields = ('uri', 'name', 'first_name', 'last_name', 'authority')
+    readonly_fields = ('uri', 'name', 'first_name', 'last_name')
+    inlines = (AttendanceInline, AffiliationInline, InvestigatorInline, LocalizationInline)
+
+
+class InstitutionAdmin(admin.ModelAdmin):
+    fields = ('uri', 'name', 'authority')
+    readonly_fields = ('uri', 'name')
+    inlines = (AffiliationInline, )
+
+
+class CourseGroupAdmin(admin.ModelAdmin):
+    fields = ('uri', 'name')
+    readonly_fields = ('uri', 'name')
+    inlines = (PartOfInline, )
+
+
+class CourseAdmin(admin.ModelAdmin):
+    fields = ('uri', 'name')
+    readonly_fields = ('uri', 'name')
+    inlines = (PartOfInline, AttendanceInline)
+
+
+class LocationAdmin(admin.ModelAdmin):
+    fields = ('uri', 'name', 'authority')
+    readonly_fields = ('uri', 'name')
+    inlines = (LocalizationInline, )
+
+
+class AffiliationAdmin(admin.ModelAdmin):
+    fields = ('year', 'person', 'institution', 'position')
+    readonly_fields = ('year', 'person', 'institution', 'position')
+
+    def get_model_perms(self, request):
+        """
+        Return empty perms dict thus hiding the model from admin index.
+        """
+        return {}    
+
+
+class AttendanceAdmin(admin.ModelAdmin):
+    fields = ('year', 'person', 'course', 'role')
+    readonly_fields = ('year', 'person', 'course', 'role')
+
+    def get_model_perms(self, request):
+        """
+        Return empty perms dict thus hiding the model from admin index.
+        """
+        return {}
+
+
+class InvestigatorAdmin(admin.ModelAdmin):
+    fields = ('year', 'person', 'subject', 'role')
+    readonly_fields = ('year', 'person', 'subject', 'role')
+
+    def get_model_perms(self, request):
+        """
+        Return empty perms dict thus hiding the model from admin index.
+        """
+        return {}
+
+
+class PartOfAdmin(admin.ModelAdmin):
+    fields = ('year', 'course', 'coursegroup')
+    readonly_fields = ('year', 'course', 'coursegroup')
+
+    def get_model_perms(self, request):
+        """
+        Return empty perms dict thus hiding the model from admin index.
+        """
+        return {}
+
+
+
+class LocalizationAdmin(admin.ModelAdmin):
+    fields = ('year', 'person', 'location')
+    readonly_fields = ('year', 'person', 'location')
+
+    def get_model_perms(self, request):
+        """
+        Return empty perms dict thus hiding the model from admin index.
+        """
+        return {}
+
+
+
+# Register your models here.
+admin.site.register(Person, PersonAdmin)
+admin.site.register(Institution, InstitutionAdmin)
+admin.site.register(Course, CourseAdmin)
+admin.site.register(CourseGroup, CourseGroupAdmin)
+admin.site.register(Location, LocationAdmin)
+admin.site.register(Affiliation, AffiliationAdmin)
+admin.site.register(Attendance, AttendanceAdmin)
+admin.site.register(PartOf, PartOfAdmin)
+admin.site.register(Localization, LocalizationAdmin)
+admin.site.register(Investigator, InvestigatorAdmin)
+admin.site.register(KnownLocation)
+admin.site.register(KnownPerson)
+admin.site.register(KnownInstitution)
---
+++ browser/management/commands/loadmbl.py
@@ -0,0 +1,122 @@
+from django.core.management.base import BaseCommand, CommandError
+from django.db.utils import IntegrityError
+from browser.models import *
+import csv
+import os
+
+def isnan(value):
+    return str(value) == 'nan' or str(value) == ''
+
+class Command(BaseCommand):
+    help = 'Load MBL data from CSV'
+
+    datatypes = [
+        ('coursegroup', 'coursegroup.csv'),
+        ('institution', 'institution.csv'),
+        ('location', 'location.csv'),
+        ('course', 'course.csv'),
+        ('person', 'person.csv'),
+        ('affiliations', 'cleaned_affiliations.csv'),
+        ('attendance', 'cleaned_coursedata.csv'),
+        ('coursegroups', 'cleaned_coursegroups.csv'),
+        ('investigators', 'cleaned_investigators.csv'),
+        ('locations', 'cleaned_locations.csv')
+    ]
+
+    def add_arguments(self, parser):
+        parser.add_argument('datapath', nargs=1, type=str)
+
+    def handle(self, *args, **options):
+        path = options['datapath'][0]
+        for name, fname in self.datatypes:
+            method = getattr(self, 'handle_{0}'.format(name))
+            for datum in self.load_csv(os.path.join(path, fname)):
+                try:
+                    method(datum)
+                except IntegrityError:
+                    pass
+
+
+    def handle_coursegroup(self, datum):
+        instance = CourseGroup(name=datum['Course Group'],
+                               uri=datum['Course Group URI'])
+        instance.save()
+
+    def handle_institution(self, datum):
+        instance = Institution(name=datum['Institution'],
+                               uri=datum['Institution URI'])
+        instance.save()
+
+    def handle_location(self, datum):
+        instance = Location(name=datum['Location'],
+                               uri=datum['Location URI'])
+        instance.save()
+
+    def handle_course(self, datum):
+        instance = Course(name=datum['Course Name'],
+                          uri=datum['Course URI'])
+        instance.save()
+
+    def handle_person(self, datum):
+        instance = Person(first_name=datum['First Name'],
+                          last_name=datum['Last Name'],
+                          uri=datum['Person URI'])
+        instance.save()
+
+    def handle_affiliations(self, datum):
+        if isnan(datum['Person URI']) or isnan(datum['Institution URI']):
+            return
+        person = Person.objects.get(uri=datum['Person URI'])
+        institution = Institution.objects.get(uri=datum['Institution URI'])
+        instance = Affiliation(person=person,
+                               institution=institution,
+                               year=datum['Year'],
+                               position=datum['Position'])
+        instance.save()
+
+    def handle_attendance(self, datum):
+        if isnan(datum['Course URI']) or isnan(datum['Person URI']):
+            return
+        person = Person.objects.get(uri=datum['Person URI'])
+        course = Course.objects.get(uri=datum['Course URI'])
+        instance = Attendance(person=person,
+                              course=course,
+                              year=datum['Year'],
+                              role=datum['Role'])
+        instance.save()
+
+    def handle_coursegroups(self, datum):
+        if isnan(datum['Course URI']) or isnan(datum['Course Group URI']):
+            return
+        course = Course.objects.get(uri=datum['Course URI'])
+        coursegroup = CourseGroup.objects.get(uri=datum['Course Group URI'])
+        instance = PartOf(course=course,
+                          coursegroup=coursegroup,
+                          year=datum['Year'])
+        instance.save()
+
+    def handle_investigators(self, datum):
+        if isnan(datum['Person URI']):
+            return
+        person = Person.objects.get(uri=datum['Person URI'])
+        instance = Investigator(person=person,
+                                year=datum['Year'],
+                                role=datum['Role'],
+                                subject=datum['Subject'])
+        instance.save()
+
+    def handle_locations(self, datum):
+        if isnan(datum['Person URI']) or isnan(datum['Location URI']):
+            return
+        person = Person.objects.get(uri=datum['Person URI'])
+        location = Location.objects.get(uri=datum['Location URI'])
+        instance = Localization(person=person,
+                                location=location,
+                                year=datum['Year'])
+        instance.save()
+
+    def load_csv(self, path):
+        with open(path, 'r') as f:
+            reader = csv.DictReader(f)
+            data = [row for row in reader]
+        return data
