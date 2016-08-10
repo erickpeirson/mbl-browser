0d7ce80dce8c1e93e0440e99994a94630c579f1f
Erick Peirson <erick.peirson@asu.edu>
2016-08-10 14:06:07 -0400

    config

--- mbl/production_settings.py
+++ mbl/production_settings.py
@@ -23,6 +23,7 @@ SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = eval(os.environ.get('DEBUG', 'False'))
+BASE_URL = os.environ.get('BASE_URL', '/')
 
 ALLOWED_HOSTS = ['.herokuapp.com']
 
