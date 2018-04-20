0ca55f2c4cdb435fe42f7cc33c08228526510518
Pranav Sharan <prnvshrn@gmail.com>
2018-04-21 03:08:30 +0530

    [MHDP-9] Add new person (#19)
    * [MHDP-9] added template, add-person view and url
    
    * [MHDP-9] added template, view for adding persons
    
    * [MHDP-9] added conceptpower_uri and description
    
    * [MHDP-9] changed DEBUG to false
    
    * [MHDP-9] made add_person view similar to edit_person view, reused change_person template
    
    * [MHDP-9] addressed review comments : resued edit_person view
    
    * [MHDP-9] addressed review comments: changed edit_person to handle_person view
    
    * [MHDP-7] addressed review comments: edited to form.save, redirecting to change_person if invalid form
    
    * [MHDP-9] addressed review comments : made changed_by hidden through forms.py
    
    * [MHDP-9] addressed review comments: Storing changed_by after post request
    
    * adjust button size

--- browser/forms.py
+++ browser/forms.py
@@ -143,7 +143,10 @@ class PersonForm(forms.ModelForm):
 
     class Meta:
         model = Person
-        fields = ['last_name', 'first_name', 'validated']
+        fields = ['last_name', 'first_name', 'validated', 'changed_by']
+        widgets = {
+            'changed_by': forms.HiddenInput()
+        }
 
 
 class DenizenMultipleChoiceField(forms.ModelMultipleChoiceField):
